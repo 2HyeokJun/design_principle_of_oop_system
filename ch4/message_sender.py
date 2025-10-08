f7a89eac7380bd8597eab6280bde4dc9f3b149c9
2HyeokJun <asymptote@live.co.kr>
2025-10-08 15:12:06 +0900

    MessageBot 클래스 추가로 인한 의존성 그룹화

--- ch4/message_sender.py
+++ ch4/message_sender.py
@@ -1,6 +1,5 @@
-from ch4.bot.bot import Bot
 from ch4.message import Message
-from ch4.user_directory import UserDirectory
+from ch4.message_bot import MessageBot
 from ch4.email_sender import EmailSender
 from ch4.user_preferences import UserPreferences
 from ch4.repository.message_repository import MessageRepository
@@ -9,16 +8,13 @@ from ch4.repository.message_repository import MessageRepository
 class MessageSender:
     def __init__(
         self,
-        bot: Bot,
-        user_directory: UserDirectory,
+        message_bot: MessageBot,
         repository: MessageRepository,
         email_sender: EmailSender,
         user_prefs: UserPreferences,
     ):
-        self.bot = bot
-        self.user_directory = user_directory
+        self.message_bot = message_bot
         self.repository = repository
-        # 새로운 의존성을 추가한다.
         self.email_sender = email_sender
         self.user_prefs = user_prefs
 
@@ -27,14 +23,7 @@ class MessageSender:
         message_to_be_sent: Message
         # 송신해야 하는 모든 메시지에 대해
         for message_to_be_sent in messages_to_be_sent:
-            # 이메일에서 사용자의 id를 가져온다.
-            user_id = self.user_directory.get_account(message_to_be_sent.get_email())
-            # 봇을 통해 메시지를 전송한다.
-            self.bot.send_private_message(
-                id=user_id,
-                message=message_to_be_sent.get_body_in_markdown(),
-            )
-            # 두가지 새 의존성을 사용해 메시지 복사본을 사용자에게 이메일로 보낼지 결정한다.
+            self.message_bot.send(message_to_be_sent)
             if self.user_prefs.send_via_email(message_to_be_sent.get_email()):
                 self.email_sender.send_message(message_to_be_sent)
 
